de163b1a609ea9a9632a5db1978be84c60767f86
Atalaa <atala.rchidi@gmail.com>
2024-02-28 17:20:50 -0500

    renamed file, handled exceptions

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
-import random
+import random, sys
 
 hangman_stages = ['''
   ============''', '''
@@ -77,25 +77,32 @@ def hangman():
     used_life = 0
     while "_" in blank_list:
         # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-        guess = input("Guess a letter: ").lower()
+        try:
+            guess = input("Guess a letter: ").lower()
 
-        # Check if guess is one of the letters in the chosen_word.
-        if guess in chosen_word:
-            for i in range(len(chosen_word)):
-                if chosen_word[i] == guess:
-                    blank_list[i] = guess
-            print(final_blank_letters.join(blank_list), "\n")
-        else:
-            # Track the count of lives used
-            if used_life < 9:
-                print(hangman_stages[used_life])
-                used_life += 1
+            # Check if guess is one of the letters in the chosen_word.
+            if guess in chosen_word:
+                for i in range(len(chosen_word)):
+                    if chosen_word[i] == guess:
+                        blank_list[i] = guess
+                print(final_blank_letters.join(blank_list), "\n")
             else:
-                print(hangman_stages[used_life])
-                print(f"YOU LOSE 🤦‍. The hidden word was '{chosen_word}'️. \n")
+                # Track the count of lives used
+                if used_life < 9:
+                    print(hangman_stages[used_life])
+                    used_life += 1
+                else:
+                    print(hangman_stages[used_life])
+                    print(f"YOU LOSE 🤦‍. The hidden word was '{chosen_word}'️. \n")
+
+        except KeyboardInterrupt:
+            print("\nHello User, you interrupted the program. Please run me again.")
+            sys.exit(0)
+        except EOFError:
+            print("\nHello User, it is EOF exception, please run me again.")
+            sys.exit(0)
 
     print("YOU WON 🏆🏆!")
 
 
 hangman()
-
